59ca07fe05fe2e989702759930290a4359237363
cv-bit <ververcameron29@gmail.com>
2022-07-18 00:54:39 -0500

    adding hero card and styling newstart

---
+++ src/components/herocard/HeroCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './herocard.css'
+
+const HeroCard = () => {
+    return (
+        <div className='herocard-container flex-center-center'>
+            HeroCard
+        </div>
+    )
+}
+
+export default HeroCard
--- src/pages/newStart/NewStart.js
+++ src/pages/newStart/NewStart.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import HeroCard from '../../components/herocard/HeroCard'
 
 import './newStart.css'
 
 const NewStart = () => {
   return (
-    <div className='newStart-container min-width-100 min-height-100 flex-center-center'>
-      <div className='text-center'>
+    <div className='newStart-container min-width-100 min-height-100 flex-center-center column'>
+      <div className='newStart-hero-container flex-center-center wrap text-center'>
+        <h2>Pick your heros</h2>
+        <HeroCard />
+        <HeroCard />
+        <HeroCard />
+        <HeroCard />
+        <HeroCard />
+        <HeroCard />
+      </div>
+      <div className='text-center height-100'>
         <h2>new game dialog</h2>
         <p>here you will be able to create your party to adventure with</p>
         <Link className='create-party-link' to="/beginning" >create a party</Link>
