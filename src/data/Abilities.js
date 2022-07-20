a779ae8baf3100a23148285c73e8dbd784947b78
cv-bit <ververcameron29@gmail.com>
2022-07-19 21:04:27 -0500

    comments on abilities starting to flesh that out still want to finish combat and enemies and other stuff before I work on that tho

---
+++ src/data/Abilities.js
@@ -0,0 +1,4 @@
+// i guess i can start making a list of abilities here
+// spell steal, firebolt, cross slash, force push, dash, teleport, take aim, haste, protect/shell, attack boost, heal, first aid
+//
+//abilities will have requirements
