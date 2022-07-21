47a6eff086d3634e154b34019400975ba4fd1e06
cv-bit <ververcameron29@gmail.com>
2022-07-21 00:34:08 -0500

    adding hero functions and ability to increase stats on character selection

---
+++ src/data/hero/HeroFunctions.js
@@ -0,0 +1,86 @@
+const plusHp = (hero) => {
+    if(localStorage.getItem('hero')) {
+        let myHero = JSON.parse(localStorage.getItem('hero'))
+        myHero.stats.hp++
+        hero.stats.hp++
+        localStorage.setItem('hero', JSON.stringify(myHero))
+    } else {
+        hero.stats.hp++
+    }
+}
+
+const plusPower = (hero) => {
+    if(localStorage.getItem('hero')) {
+        let myHero = JSON.parse(localStorage.getItem('hero'))
+        myHero.stats.power++
+        hero.stats.power++
+        localStorage.setItem('hero', JSON.stringify(myHero))
+    } else {
+        hero.stats.power++
+    }
+}
+
+const plusMagic = (hero) => {
+    if(localStorage.getItem('hero')) {
+        let myHero = JSON.parse(localStorage.getItem('hero'))
+        myHero.stats.magic++
+        hero.stats.magic++
+        localStorage.setItem('hero', JSON.stringify(myHero))
+    } else {
+        hero.stats.magic++
+    }
+}
+
+const plusSpeed = (hero) => {
+    if(localStorage.getItem('hero')) {
+        let myHero = JSON.parse(localStorage.getItem('hero'))
+        myHero.stats.speed++
+        hero.stats.speed++
+        localStorage.setItem('hero', JSON.stringify(myHero))
+    } else {
+        hero.stats.speed++
+    }
+}
+
+const plusStamina = (hero) => {
+    if(localStorage.getItem('hero')) {
+        let myHero = JSON.parse(localStorage.getItem('hero'))
+        myHero.stats.stamina++
+        hero.stats.stamina++
+        localStorage.setItem('hero', JSON.stringify(myHero))
+    } else {
+        hero.stats.stamina++
+    }
+}
+
+const plusLuck = (hero) => {
+    if(localStorage.getItem('hero')) {
+        let myHero = JSON.parse(localStorage.getItem('hero'))
+        myHero.stats.luck++
+        hero.stats.luck++
+        localStorage.setItem('hero', JSON.stringify(myHero))
+    } else {
+        hero.stats.luck++
+    }
+}
+
+const plusXp = (hero) => {
+    if(localStorage.getItem('hero')) {
+        let myHero = JSON.parse(localStorage.getItem('hero'))
+        myHero.stats.xp++
+        hero.stats.xp++
+        localStorage.setItem('hero', JSON.stringify(myHero))
+    } else {
+        hero.stats.xp++
+    }
+}
+
+export {
+    plusHp,
+    plusPower,
+    plusMagic,
+    plusSpeed,
+    plusStamina,
+    plusLuck,
+    plusXp
+}
--- src/data/hero/heromodal/HeroModal.js
+++ src/data/hero/heromodal/HeroModal.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { diceRoller } from '../../../utils/DiceRoller'
+import {HiOutlinePlus} from 'react-icons/hi'
+import { plusPower, plusMagic, plusSpeed, plusLuck } from '../HeroFunctions'
 
 import './heromodal.css'
 
@@ -21,21 +23,34 @@ const HeroModal = ({hero, setHeroModal}) => {
             <button className='heromodal-stat-btn' onClick={() => hasRolled && (setRollResuluts(diceRoller(6, statRolls)) & setHasRolled(false))}>roll for stat points</button>
             <p>you can roll once</p>
             <h2>{hero.name}</h2>
-            {rollResults > 0 && <p>stat points to distribute {rollResults}</p>}
+            
             <p>customize your hero with stat points</p>
+            <br/>
+            {rollResults > 0 && <p>stat points to distribute {rollResults}</p>}
+
             <div className=''>
-                <div>
-                    <p>power: {hero.stats.power}</p>
-                    <p>magic: {hero.stats.magic}</p>
-                    <p>speed: {hero.stats.speed}</p>
-                    <p>luck: {hero.stats.luck}</p>
+                <div className='flex-center-center column width-100'>
+                    <div className='flex width-100 margin-5'>
+                        <p>power: {hero.stats.power}</p> <HiOutlinePlus className='stat-plus' onClick={() => rollResults > 0 && plusPower(hero) & setRollResuluts(rollResults -1)}/> 
+                    </div>
+                    <div className='flex width-100 margin-5'>
+                        <p>magic: {hero.stats.magic}</p> <HiOutlinePlus className='stat-plus' onClick={() => rollResults > 0 && plusMagic(hero) & setRollResuluts(rollResults -1)}/>
+                    </div>
+                    <div className='flex width-100 margin-5'>
+                        <p>speed: {hero.stats.speed}</p> <HiOutlinePlus className='stat-plus' onClick={() => rollResults > 0 && plusSpeed(hero) & setRollResuluts(rollResults -1)}/>
+                    </div> 
+                    <div className='flex width-100 margin-5'>
+                        <p>luck: {hero.stats.luck}</p> <HiOutlinePlus className='stat-plus' onClick={() => rollResults > 0 && plusLuck(hero) & setRollResuluts(rollResults -1)}/> 
+                    </div>
                 </div>
                 <div>
                 
                 </div>
             </div>
-            <button className='heromodal-close-btn' onClick={() => setHeroModal(false) }>close</button>
-            <button className='heromodal-save-btn' onClick={() => saveHero()} >save hero</button>
+            <div>
+                <button className='heromodal-close-btn' onClick={() => setHeroModal(false) }>close</button>
+                <button className='heromodal-save-btn' onClick={() => saveHero()} >save hero</button>
+            </div>
         </div>
     )
 }
