13e28db6d0356913f671da820fa946366c32572d
cv-bit <ververcameron29@gmail.com>
2022-07-21 01:55:13 -0500

    update starting health

--- src/data/hero/HeroList.js
+++ src/data/hero/HeroList.js
@@ -5,7 +5,7 @@ const HeroOne = new HeroClass()
 HeroOne.name = 'hero 1'
 HeroOne.focus = 'combat'
 HeroOne.stats= {
-    hp: 0,
+    hp: 5,
     power: 2,
     magic: 0,
     speed: 0,
@@ -19,7 +19,7 @@ const HeroTwo = new HeroClass()
 HeroTwo.name = 'hero 2'
 HeroTwo.focus = 'combat'
 HeroTwo.stats= {
-    hp: 0,
+    hp: 4,
     power: 0,
     magic: 0,
     speed: 2,
@@ -33,7 +33,7 @@ const HeroThree = new HeroClass()
 HeroThree.name = 'hero 3'
 HeroThree.focus = 'combat'
 HeroThree.stats= {
-    hp: 0,
+    hp: 3,
     power: 0,
     magic: 2,
     speed: 0,
@@ -47,7 +47,7 @@ const HeroFour = new HeroClass()
 HeroFour.name = 'hero 4'
 HeroFour.focus = 'support'
 HeroFour.stats= {
-    hp: 0,
+    hp: 3,
     power: 0,
     magic: 0,
     speed: 1,
@@ -61,7 +61,7 @@ const HeroFive = new HeroClass()
 HeroFive.name = 'hero 5'
 HeroFive.focus = 'support'
 HeroFive.stats= {
-    hp: 0,
+    hp: 4,
     power: 0,
     magic: 1,
     speed: 1,
@@ -75,7 +75,7 @@ const HeroSix = new HeroClass()
 HeroSix.name = 'hero 6'
 HeroSix.focus = 'support'
 HeroSix.stats= {
-    hp: 0,
+    hp: 5,
     power: 1,
     magic: 0,
     speed: 1,
