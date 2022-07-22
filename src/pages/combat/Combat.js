91871551e530dd19652b2badc815114a74406cb8
cv-bit <ververcameron29@gmail.com>
2022-07-21 23:23:38 -0500

    update grammer

--- src/pages/combat/Combat.js
+++ src/pages/combat/Combat.js
@@ -8,7 +8,7 @@ const Combat = () => {
     return (
         <div className='combat-container min-width-100 min-height-100 flex-center-center column text-center'>
             <h2>Arena</h2>
-            <p>here you can fight monsters to gain fame, gold, and items, and even unlock story cards</p>
+            <p>here you can fight monsters to gain fame, gold, items, and even unlock story cards</p>
             <div className='flex-center-center wrap text-center'>
                 {EnemyList.map((enemy, key) => {
                     return <EnemyCard enemy={enemy} key={key} />
