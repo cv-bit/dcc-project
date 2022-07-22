e6febaa046b5a40c4fb7334d7e61f49f751ee66f
cv-bit <ververcameron29@gmail.com>
2022-07-21 22:55:07 -0500

    style updates

--- src/pages/guild/Guild.js
+++ src/pages/guild/Guild.js
@@ -4,7 +4,7 @@ import './guild.css'
 
 const Guild = () => {
   return (
-    <div className='guild-container min-width-100 min-height-100 flex-center-center column'>
+    <div className='guild-container min-width-100 min-height-100 flex-center-center column text-center'>
         <h2>Guild</h2>
         <p>coming soon</p>
         <p>from here you can aquire companions unlock story cards and go through training to learn cool abilities</p>
--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -4,7 +4,7 @@ import './shop.css'
 
 const Shop = () => {
   return (
-    <div className='shop-container min-width-100 min-height-100 flex-center-center column'>
+    <div className='shop-container min-width-100 min-height-100 flex-center-center column text-center'>
         <h2>Shop</h2>
         <p>coming soon</p>
         <p>here you can buy all kinds of cool stuff from weapons to armor, companions and even story cards</p>
