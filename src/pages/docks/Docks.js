f0cc4f7753e70b330a10efd5acbdc81211446f6b
cv-bit <ververcameron29@gmail.com>
2022-07-23 17:05:15 -0500

    update styles

--- src/pages/docks/Docks.js
+++ src/pages/docks/Docks.js
@@ -4,7 +4,7 @@ import './docks.css'
 
 const Docks = () => {
   return (
-    <div className='docks-container min-width-100 min-height-100 flex-center-center column'>
+    <div className='docks-container min-width-100 min-height-100 flex-center-center column text-center'>
         <h2>Docks</h2>
         <p>coming soon</p>
         <p>here you can go to other cities and find new adventures</p>
