24658bdeeb36d73b9b580d5b23e8ef5247b38503
cv-bit <ververcameron29@gmail.com>
2022-07-25 22:45:52 -0500

    add more buttons to combat screen and adjust item function

--- src/pages/combat/Combat.js
+++ src/pages/combat/Combat.js
@@ -27,35 +27,48 @@ const Combat = () => {
     }
 
     const Items = () => {
+        let itemList = []
         if(hero.items.length < 1) alert('you have no items')
-        if(hero.items.length > 0) alert(JSON.stringify(hero.items))
+        if(hero.items.length > 0) {
+            hero.items.forEach(item => {
+                itemList.push(item.name)
+            });
+            alert('items: ' + itemList)
+        }
     }
 
-  return (
-    <div className='combat-container min-width-100 min-height-100 flex-align-center column text-center'>
-    <Link className='home-btn' to='/beginning'>Flee</Link>
-    <h2>Battle</h2>
-        <div className='enemy-battle-container flex-center-center'>
-            { enemy && enemy.stats.hp &&
-                <div className="enemy-info-container">
-                    <img src={enemy.img} alt="enemy img" className="enemy-img" />
-                    <h2>{enemy.name}</h2>
-                    <p>hp: {enemy.tempStats.tempHp - enemyDamage}/{enemy.stats.hp}</p>
+    return (
+        <div className='combat-container min-width-100 min-height-100 flex-align-center column text-center'>
+        <Link className='home-btn' to='/beginning'>Flee</Link>
+        <h2>Battle</h2>
+            <div className='enemy-battle-container flex-center-center'>
+                { enemy && enemy.stats.hp &&
+                    <div className="enemy-info-container">
+                        <img src={enemy.img} alt="enemy img" className="enemy-img" />
+                        <h2>{enemy.name}</h2>
+                        <p>hp: {enemy.tempStats.tempHp - enemyDamage}/{enemy.stats.hp}</p>
+                    </div>
+                }
+            </div>
+            <div className='hero-control-bar flex-align-center'>
+                <div>
+                    <h2>{hero.name}</h2>
+                    <p>hp: {hero.tempStats.tempHp - playerDamage}/{hero.stats.hp}</p>
                 </div>
-            }
-        </div>
-        <div className='hero-control-bar flex-align-center'>
-            <div>
-                <h2>{hero.name}</h2>
-                <p>hp: {hero.tempStats.tempHp - playerDamage}/{hero.stats.hp}</p>
+                <div>
+                    <button className='combat-s-attack-btn' >Strong Attack</button>
+                    <button className='combat-d-attack-btn'>Double Strike</button>
+                </div>
+                <div>
+                    <button className='combat-attack-btn' onClick={() => Attack()}>Attack</button>
+                    <button className='combat-item-btn' onClick={() => Items()}>Items</button>
+                </div>
+                <div>
+                    <button className='combat-magic-btn'>Magic</button>
+                </div>
             </div>
-            <div>
-                <button className='combat-attack-btn' onClick={() => Attack()}>Attack</button>
-                <button className='combat-item-btn' onClick={() => Items()}>Items</button>
-            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Combat
