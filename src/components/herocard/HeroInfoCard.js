c1b406ae7c8ff6eb46ce83757bb5b94789ff9f8e
cv-bit <ververcameron29@gmail.com>
2022-07-27 00:45:24 -0500

    adding scroll to item list heroinfocard

--- src/components/herocard/HeroInfoCard.js
+++ src/components/herocard/HeroInfoCard.js
@@ -4,11 +4,13 @@ const HeroInfoCard = ({hero}) => {
   return (
     <div className='heroinfo-container flex-center-center column text-center'>
         <h2>hero inventory</h2>
-        {
+        <div className='heroinfo-item-container'>
+          {
             hero.items.length > 0 ? hero.items.map((item, key) => {
                 return <p key={key}>{item.name}</p>
             }) : <p >go to the market to get stuff you don't have anything</p>
-        }
+          }
+        </div>
         <p style={{color: 'yellow'}}>gold: {hero.gold}</p>
     </div>
   )
